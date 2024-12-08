5271ff19bde4c0b64704c21dffa658046fe323d6
brovkin <andrey.brovkin.1995@mail.ru>
2024-12-08 18:29:00 +0300

    black, isort, mypy fixes + new main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-print("Введите номер карты")
-card_number = input()
-print("Введите аккаунт")
-account = input()
-masked_card_number = get_mask_card_number(card_number)
-masked_account = get_mask_account(account)
-print(f"Замаскированный номер: {masked_card_number}")
-print(f"Замаскированный аккаунт: {masked_account}")
+if __name__ == "__main__":
+    print("Введите аккаунт")
+    card_number = input()
+    masked_result = mask_account_card(card_number)
+    print(f"Маска: {masked_result}")
--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,17 @@
-from masks import get_mask_account, get_mask_card_number
 from datetime import datetime
 
-def mask_account_card(account: str)->str:
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(account: str) -> str:
     """Функция маскировки аккаунта/счета"""
-    masked_number_list = list()
     first_number_index = 0
     for index in range(len(account)):
         if account[index].isdigit():
             first_number_index = index
             break
     masked_number = account[first_number_index:]
-    if 'счет' in account.lower():
+    if "счет" in account.lower():
         masked_deposit_number = get_mask_account(masked_number)
         masked_deposit = account[:first_number_index] + masked_deposit_number
         return masked_deposit
@@ -25,4 +26,3 @@ def get_date(date_string):
     date_object = datetime.fromisoformat(date_string)
     formatted_date = date_object.strftime("%d.%m.%Y")
     return formatted_date
-
