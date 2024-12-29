dd50ab72ef5c1a615e5eafb43a7c26cd26ee9f8c
brovkin <andrey.brovkin.1995@mail.ru>
2024-12-29 22:03:58 +0300

    tests done + little fix, isort + black + mypy + flake8 fixes

--- tests/tests_generators.py
+++ tests/tests_generators.py
@@ -1,7 +1,6 @@
 import pytest
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
-
 @pytest.mark.parametrize("currency, expected_ids", [
     ('USD', [939719570, 142264268, 111111112]),
     ('EUR', [123456789]),
@@ -21,19 +20,80 @@ def test_filter_by_currency_empty_list():
     assert len(result) == 0
 
 
-def test_transaction_descriptions(transaction_data):
-    expected_descriptions = [
-        "Перевод организации",
-        "Перевод со счета на счет",
-        "Перевод в евро",
-        "Перевод в йенах",
-        "Небольшой перевод"
-    ]
+@pytest.mark.parametrize("transaction_data, expected_descriptions", [
+    (
+        [
+            {'description': "Перевод организации"},
+            {'description': "Перевод со счета на счет"},
+            {'description': "Перевод в евро"},
+            {'description': "Перевод в йенах"},
+            {'description': "Небольшой перевод"}
+        ],
+        [
+            "Перевод организации",
+            "Перевод со счета на счет",
+            "Перевод в евро",
+            "Перевод в йенах",
+            "Небольшой перевод"
+        ]
+    ),
+    (
+        [
+            {'description': "Тестовый перевод 1"},
+            {'description': "Тестовый перевод 2"}
+        ],
+        [
+            "Тестовый перевод 1",
+            "Тестовый перевод 2"
+        ]
+    ),
+    (
+        [{'description': "Перевод без описания"}],
+        ["Перевод без описания"]
+    ),
+    (
+        [{'description': ""}],
+        [""]
+    ),
+    (
+        [],
+        []
+    )
+])
+
+def test_transaction_descriptions(transaction_data, expected_descriptions):
     result = list(transaction_descriptions(transaction_data))
     assert result == expected_descriptions
 
-def test_transaction_descriptions_empty():
-    result = list(transaction_descriptions([]))
-    assert result == []
+@pytest.mark.parametrize("start, end, expected", [
+    (0, 5, [
+        "0000 0000 0000 0000",
+        "0000 0000 0000 0001",
+        "0000 0000 0000 0002",
+        "0000 0000 0000 0003",
+        "0000 0000 0000 0004",
+        "0000 0000 0000 0005"
+    ]),
+    (10, 15, [
+        "0000 0000 0000 0010",
+        "0000 0000 0000 0011",
+        "0000 0000 0000 0012",
+        "0000 0000 0000 0013",
+        "0000 0000 0000 0014",
+        "0000 0000 0000 0015"
+    ]),
+    (9995, 10000, [
+        "0000 0000 0000 9995",
+        "0000 0000 0000 9996",
+        "0000 0000 0000 9997",
+        "0000 0000 0000 9998",
+        "0000 0000 0000 9999",
+        "0000 0000 0001 0000"
+    ])
+])
+
+def test_card_number_generator(start, end, expected):
+    result = list(card_number_generator(start, end))
+    assert result == expected
 
 
