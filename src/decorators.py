c166cdb61ec89470afa3afb958a96745fd5c0a96
brovkin <andrey.brovkin.1995@mail.ru>
2025-01-06 02:18:33 +0300

    flake8 mypy black isort fixes

--- src/decorators.py
+++ src/decorators.py
@@ -2,7 +2,6 @@ def log(filename=None):
     def decorator(func):
         def wrapper(*args, **kwargs):
             error_occured = False
-            error_name = ""
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{func.__name__} ok"
@@ -12,16 +11,14 @@ def log(filename=None):
                 error_occured = True
                 error = e
             if filename:
-                with open(filename, 'a') as f:
-                    f.write(log_message + '\n')
+                with open(filename, "a") as f:
+                    f.write(log_message + "\n")
             else:
                 print(log_message)
-            if error_occured == True:
+            if error_occured is True:
                 raise error
             return result
+
         return wrapper
+
     return decorator
-
-
-
-
