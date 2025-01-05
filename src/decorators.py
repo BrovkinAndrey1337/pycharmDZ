15028d2a1377b61882fb84dbb324306717b169a0
brovkin <andrey.brovkin.1995@mail.ru>
2025-01-06 02:15:28 +0300

    added decorator

---
+++ src/decorators.py
@@ -0,0 +1,27 @@
+def log(filename=None):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            error_occured = False
+            error_name = ""
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+            except Exception as e:
+                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                result = None
+                error_occured = True
+                error = e
+            if filename:
+                with open(filename, 'a') as f:
+                    f.write(log_message + '\n')
+            else:
+                print(log_message)
+            if error_occured == True:
+                raise error
+            return result
+        return wrapper
+    return decorator
+
+
+
+
