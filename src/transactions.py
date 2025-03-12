e0eb6c1b469b409e65a9247a55e709c0f36f9a6b
brovkin <andrey.brovkin.1995@mail.ru>
2025-03-12 22:48:40 +0300

    added transaction.py

---
+++ src/transactions.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def read_transactions_from_csv(file_path: str):
+    """Функция считывания финансовых операций с csv файла"""
+    try:
+        transactions_info = pd.read_csv(file_path, delimiter=";")
+        return transactions_info
+    except Exception as e:
+        raise Exception(f"Произошла ошибка при чтении файла: {e}")
+
+
+def read_transactions_from_excel(file_path: str):
+    """Функция считывания финансовых операций с Excel файла"""
+    try:
+        df = pd.read_excel(file_path)
+        transactions_info = df.to_dict(orient="records")
+        return transactions_info
+    except Exception as e:
+        raise Exception(f"Произошла ошибка при чтении файла: {e}")
